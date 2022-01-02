496828152a6891443b9b4eb829f6ccdb57103271
Idan Horovitz <idanhh55@gmail.com>
2022-01-02 18:42:59 +0200

    Added examples to README.md file

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
-# TaskScheduler
+# TasksScheduler
+
 ---
-SaskScheduler is a system which loads runs scheduled tasks by using a cron-like pattern
+TasksScheduler is a system which runs scheduled tasks by using a cron-like pattern
 
 ## Description
+
 ---
 TasksScheduler automatically executes scheduled tasks.
 The system loads tasks from a CSV file and executes them automatically when scheduled.
-The tasks are executed sequentially and also supports simultaneously executions by using a multithreaded architecture.
+The tasks are executed sequentially and also supports simultaneous executions by using a multithreaded architecture.
 
 #### Task - 
 ```c++
-typedef struct Task {
+struct Task {
     std::string command;
     cron::cronexpr cron_expression;
     std::time_t next_execution;
@@ -19,11 +21,6 @@ typedef struct Task {
 
 The CSV file has 4 columns which represent the command and the time in which it needs to be executed.
 
-Value range:
-- Minute: 0-59
-- Hour: 0-23
-- Day of week: 0-6, where 0 = Monday, 6 = Sunday
-- Command: text
 
 #### CSV file structure: 
 minute | hour | day_of_week  |  command
@@ -38,26 +35,23 @@ minute | hour | day_of_week  |  command
 
 #### The time at this run - Sunday 18:34 for CSV the example
 
-#### Task  4 and 5 should execute.
+#### Task 4 and 5 should execute one after the other.
 ![CSV_example](./images/CSV_example.png)
 #### Scheduler start:
 ![scheduler_start](./images/scheduler_start.png) 
 #### Task 4 started and changed to sleep for 2 minutes:
 ![task4_start_and_sleep](./images/task4_start_and_sleep.png)
-#### Task 5 arrive and execute before task 4: 
+#### Task 5 arrives and executes before task 4: 
 ![task5_start_and_execute](./images/task5_start_and_execute.png)
-#### Task 4 finish his sleep time and executed:
+#### Task 4 finish its sleep time and executed:
 ![task4_execute](./images/task4_execute.png)
 
-# Installation:
+# Dependencies:
+
+---
 This project was written using Microsoft Visual Studio 2019 and C++14, other versions may not work.
-```
-gcc source.cpp -o TasksScheduler.exe
-```
-Then run:
-```
-TasksScheduler.exe
-```
 
 ## Credits:
+
+---
 `croncpp.h`: https://github.com/mariusbancila/croncpp
--- Source.cpp
+++ Source.cpp
@@ -7,56 +7,68 @@
 #include <queue>
 #include <thread>
 #include <chrono>
-#include <future>
 
 #include "croncpp.h" // Taken from https://github.com/mariusbancila/croncpp for cron pattern
 
 
-typedef struct Task {
+struct Task {
     std::string command;
     cron::cronexpr cron_expression;
     std::time_t next_execution;
 
-    bool operator<(Task const& other_task) { return next_execution < other_task.next_execution; }
-    friend std::ostream& operator<<(std::ostream& os, const Task& task) {
-        return os << "The task: " << task.command << "\n" << " execute at: " << std::put_time(localtime(&task.next_execution), "%FT%H:%M");
+    bool operator<(Task const &other_task) {
+        return next_execution < other_task.next_execution;
+    } // Used in order to sort the queue.
+
+    friend std::ostream &operator<<(std::ostream &os, const Task &task) {
+        return os << "The task: " << task.command << "\n" << " execute at: "
+                  << std::put_time(localtime(&task.next_execution), "%FT%H:%M");
     };
 };
 
 int Start();
-void ParseTasksCSV(std::deque<Task>&, std::string&);
-Task CreateTask(std::string&, std::string&, std::string&, std::string&);
-void ExecuteTasks(std::deque<Task>&);
+
+void ParseTasksCSV(std::deque <Task> &, std::string &);
+
+Task CreateTask(std::string &, std::string &, std::string &, std::string &);
+
+void ExecuteTasks(std::deque <Task> &);
+
 void ExecuteTask(std::string);
+
+const char *const DAYS[] = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
 
 int main() {
     return Start();
 }
 
 /**
- * Loading the CSV file and and initialize the queue of tasks,
- * then calls functions in chronological order.
+ * Runs TasksScheduler.
  */
 int Start() {
+    auto now{std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())};
     std::string file_name = "./tasks.csv";
-    std::deque<Task> tasks_queue;
-    std::cout << "The scheduler started to run: " << std::endl;
+    std::deque <Task> tasks_queue;
+
+    std::cout << "The scheduler started to run at: " << std::put_time(localtime(&now), "%FT%H:%M") << std::endl;
     ParseTasksCSV(tasks_queue, file_name);
     ExecuteTasks(tasks_queue);
+
     return 0;
 }
 
 /**
  * Parse the CSV file and populate the tasks queue.
-
  * 
  * @param tasks_queue - An empty queue which stores the tasks.
  * @param file_name - The path to the tasks CSV.
  */
-void ParseTasksCSV(std::deque<Task>& tasks_queue, std::string& file_name) {
+void ParseTasksCSV(std::deque <Task> &tasks_queue, std::string &file_name) {
     std::ifstream tasks_csv(file_name);
-    if (!tasks_csv.is_open()) std::cerr << "ERROR: Could not open file" << '\n'; // TODO: Add exit
-
+    if (!tasks_csv.is_open()) {
+        std::cerr << "ERROR: Could not open file" << '\n';
+        exit(1);
+    }
     std::string minute, hour, day_of_week, command;
     getline(tasks_csv, minute, '\n'); // Ignores the first row (CSV title) 
 
@@ -68,9 +80,10 @@ void ParseTasksCSV(std::deque<Task>& tasks_queue, std::string& file_name) {
         tasks_queue.push_front(CreateTask(minute, hour, day_of_week, command));
     }
 
-    std::sort(std::begin(tasks_queue), std::end(tasks_queue));
     tasks_csv.close();
+    std::sort(std::begin(tasks_queue), std::end(tasks_queue));
 }
+
 /**
  * Returns a Task object, built from received parameters and the calculated next execution time.
  * 
@@ -80,42 +93,52 @@ void ParseTasksCSV(std::deque<Task>& tasks_queue, std::string& file_name) {
  * @param command - The command which will be executed.
  * @return task - The constructed task
  */
-Task CreateTask(std::string& minute, std::string& hour, std::string& day_of_week, std::string& command) {
-    std::string cron_str = "* " + minute + " " + hour + " * * " + day_of_week; // cron expression represnting: at H:M AM, only on (Day of week)
+Task CreateTask(std::string &minute, std::string &hour, std::string &day_of_week, std::string &command) {
+    std::string cron_str = "* " + minute + " " + hour + " * * " + DAYS[stoi(day_of_week)];
+    // cron expression representing: at H:M AM, only on (Day of week)
     auto cron = cron::make_cron(cron_str);
-    Task task{ command, cron, cron::cron_next(cron, std::time(0)) };
+    Task task{command, cron, cron::cron_next(cron, std::time(0))};
+
     return task;
 }
+
 /**
  * Execute the given command as system call.
-
  *
  * @param command - The command
  */
 void ExecuteTask(std::string command) {
     system(command.c_str());
-    auto now{ std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()) };
-    std::cout << "The task: " << command << "\n" << " execute at: " << std::put_time(localtime(&now), "%FT%H:%M") << std::endl;
-    
+    auto now{std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())};
+    std::cout << "The task: " << command << "\n" << " execute at: "
+    << std::put_time(localtime(&now), "%FT%H:%M") << std::endl;
+
 }
+
 /**
- * Pulls tasks while the queue is not empty,
- * changing the thread to sleep until his exection time,
- * executing the tasks in diffrent threads,
- * then pushes the task to the end of the queue to create circle of tasks.
+ * Fetches tasks from the task queue and runs them according to their execution time
+ * The tasks are then returned to the bottom of the queue in order to create a never-ending
+ * cycle of tasks (Possible due to the fact that they all run once a week)
  *
  * @param tasks_queue - A queue full of tasks waiting to be executed.
  */
-void ExecuteTasks(std::deque<Task>& tasks_queue) {
-    std::vector<std::thread> workers;
+void ExecuteTasks(std::deque <Task> &tasks_queue) {
+    std::vector <std::thread> workers;
 
     while (!tasks_queue.empty()) {
         Task current_task = tasks_queue.front();
-        std::this_thread::sleep_until(std::chrono::system_clock::from_time_t(current_task.next_execution)); //The time that thread should wait until it's scheduled time.
+        Task next_task = current_task;
+
+        std::cout << "The next task will run at: "
+        << std::put_time(localtime(&next_task.next_execution), "%FT%H:%M") << std::endl;
+
+        next_task.next_execution = cron::cron_next(next_task.cron_expression, next_task.next_execution);
+        std::this_thread::sleep_until(std::chrono::system_clock::from_time_t(current_task.next_execution));
+        //The time that thread should wait until it's scheduled time.
 
         workers.push_back(std::thread(ExecuteTask, current_task.command));
-        tasks_queue.push_back(current_task);
+        tasks_queue.push_back(next_task);
         tasks_queue.pop_front();
     }
-    
+
 }
